7e47cb5dbc57c72b6e85ea3666a8f945c3181e48
aikuangyong <15016726180@139.com>
2021-03-26 17:54:00 +0800

    多环境配置

--- src/main/java/com/zywl/demo/login/test/TestController.java
+++ src/main/java/com/zywl/demo/login/test/TestController.java
@@ -1,6 +1,8 @@
 package com.zywl.demo.login.test;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
+
+    protected static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @RequestMapping("/hello")
     @ResponseBody
     public String test() {
+        logger.info("===系统调用了TestController3===");
         return "HelloWorld";
     }
 }
